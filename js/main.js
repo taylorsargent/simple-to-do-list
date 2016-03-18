06450bc823b09e73ed767745c2188d85e3711216
Taylor Sargent <sarg0049@algonquinlive.com>
2016-03-18 10:16:57 -0400

    made files added js

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var $input = $('#todo-input');
+var $lsit = $('.list');
+
+$('form').on('submit', function(e) {
+  var $li = $('<li>');
+
+  e.preventDefult();
+  $li.html($input.val());
+  $input.val('');
+  $list.append($li);
+});
+
+$('ul').on('click', 'li', function (e){
+  $
+})

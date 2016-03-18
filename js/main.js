4dc292f50e9f617802c25388938b05235c164748
Taylor Sargent <sarg0049@algonquinlive.com>
2016-03-18 10:21:33 -0400

    fixed markbot errors

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 var $input = $('#todo-input');
 var $lsit = $('.list');
 
-$('form').on('submit', function(e) {
+$('form').on('submit', function (e) {
   var $li = $('<li>');
 
-  e.preventDefult();
+  e.preventDefault();
   $li.html($input.val());
   $input.val('');
   $list.append($li);
 });
 
-$('ul').on('click', 'li', function (e){
+$('ul').on('click', 'li', function (e) {
   $(this).toggleClass('line');
 });
